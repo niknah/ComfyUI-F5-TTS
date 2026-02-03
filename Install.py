6478664f834bb43b0d63e85f8fd0bb46eea62ca1
niknah <github@mail.niknah.com>
2026-02-03 15:33:00 +1100

    ComfyUI-Manager no longer uses git.  We'll use git manually.
    https://github.com/niknah/ComfyUI-F5-TTS/issues/60
    https://github.com/niknah/ComfyUI-F5-TTS/issues/32

--- Install.py
+++ Install.py
@@ -17,19 +17,37 @@ class Install:
     @staticmethod
     def install():
         print("F5TTS. Checking out submodules")
-        try:
-            import pygit2
-            repo_path = os.path.join(os.path.dirname(__file__))
-            repo = pygit2.Repository(repo_path)
-            submodules = pygit2.submodules.SubmoduleCollection(repo)
-            submodules.update(init=True)
-        except Exception as e:
-            print(f"pygit2 failed: {e}")
-        subprocess.run(
-            ['git', 'submodule', 'update', '--init', '--recursive'],
-            cwd=os.path.dirname(__file__),
-            shell=True,
-            )
+        if not os.path.exists(Install.f5TTSPath):
+            os.mkdir(Install.f5TTSPath)
+        else:
+            try:
+                import pygit2
+                repo_path = os.path.join(os.path.dirname(__file__))
+                repo = pygit2.Repository(repo_path)
+                submodules = pygit2.submodules.SubmoduleCollection(repo)
+                submodules.update(init=True)
+            except Exception as e:
+                print(f"pygit2 failed: {e}")
+            subprocess.run(
+                ['git', 'submodule', 'update', '--init', '--recursive'],
+                cwd=os.path.dirname(__file__),
+                shell=True,
+                )
+
+        if not Install.has_submodule_file():
+            f5_tts_repo_url = 'https://github.com/SWivid/F5-TTS'
+            subprocess.run(
+                ['git', 'clone', '--depth=1', f5_tts_repo_url],
+                cwd=os.path.dirname(__file__),
+                shell=True,
+                )
+            path = "F5-TTS"
+            if not Install.has_submodule_file():
+                try:
+                    pygit2.clone_repository(f5_tts_repo_url, path)
+                except Exception as e:
+                    print(f"pygit2 failed: {e}")
+
         if not Install.has_submodule_file():
             print("F5TTS. Something is wrong with your git installation.  It is unable to checkout submodules.  You can install the latest from https://git-scm.com/downloads")  # noqa: E501
 
